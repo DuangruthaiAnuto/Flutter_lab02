a77723207f7d82f9f5e0fab89f2255c198126362
DUANGRUTHAI\Duangruthai <614234005@parichat.skru.ac.th>
2020-12-29 16:35:56 +0700

    scooll in lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SuperHeros());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SuperHeros extends StatelessWidget {
+  final List<String> _heroImages = [
+    "images/Flash.png",
+    "images/spydy.png",
+    "images/Thor.png",
+    "images/wonderwoman.png",
+  ];
+
+  final List<String> _heroNames = [
+    "The Flash",
+    "Spyderman",
+    "Thor",
+    "Wonder Woman",
+  ];
+
+  final List<String> _heroDetails = [
+    "The Flash is an American superhero television series developed by Greg Berlanti, Andrew Kreisberg, and Geoff Johns, airing on The CW. It is based on the Barry Allen incarnation of DC Comics character the Flash, a costumed superhero crime-fighter with the power to move at superhuman speeds.",
+    "Superhuman strength, agility, endurance, ability to stick to and climb walls and other surfaces, uses self-designed web-shooters allowing him to fire and swing from sticky webs, special 'Spider-Sense' warns of incoming danger, genius intellect specializing in chemistry and invention.",
+    "Thor is a hammer-wielding god associated with lightning, thunder, storms, sacred groves and trees, strength, the protection of mankind and also hallowing and fertility.",
+    "Wonder Woman is the princess Diana, the daughter of Hippolyta, Queen of the Amazons and Zeus, the mightiest of the Gods of Olympus. Diana volunteered to leave behind her home of Themyscira and champion the Amazons' message of peace, fighting for justice and equality in Man's World. She is a founding member of the Justice League.",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal[900],
+          title: Text("Super Hero"),
+        ),
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.all(10),
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(20),
+                    width: double.infinity,
+                    color: Colors.teal[300],
+                    child: Center(
+                      child: Text(
+                        "Super Hero",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Hero(this._heroNames[0], this._heroImages[0],
+                      this._heroDetails[0]),
+                  Hero(this._heroNames[1], this._heroImages[1],
+                      this._heroDetails[1]),
+                  Hero(this._heroNames[2], this._heroImages[2],
+                      this._heroDetails[2]),
+                  Hero(this._heroNames[3], this._heroImages[3],
+                      this._heroDetails[3]),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Hero extends StatelessWidget {
+  String _heroNames;
+  String _heroImages;
+  String _heroDetails;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Hero(this._heroNames, this._heroImages, this._heroDetails, {Key key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Card(
+      child: Container(
+        color: Colors.teal[100],
+        width: double.infinity,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            SizedBox(
+              height: 10,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(this._heroImages),
             ),
+            SizedBox(height: 10),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.flash_on),
+                  Text(
+                    this._heroNames,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.pink[900],
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Text(this._heroDetails),
+              padding: EdgeInsets.all(25),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
